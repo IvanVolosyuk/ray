2d34803f1529bd66c11aa923b8a918047d94be4f
Ivan Volosyuk <Ivan.Volosyuk@gmail.com>
2018-07-08 19:21:24 +1000

    Added various options and motion

--- src/ray.cpp
+++ src/ray.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <vector>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 #include "ray.hpp"
 #include "vector.hpp"
@@ -12,35 +15,40 @@
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 600
 
-#define P(x) print(#x, x);
-// #define P(x) {}
+//#define P(x) print(#x, x);
+#define P(x) {}
 
 
 using Vector = Point;
 #define optional boost::optional
 #define nullopt boost::none
 
-Point light_pos = Point(-4.2, 0, 5);
-float light_size = 3.0;
+Point light_pos = Point(-4.2, -3, 2);
+float light_size = 2.0;
 float light_power = 3.4;
 Point light_color = Point(light_power, light_power, light_power);
 float light_size2 = light_size * light_size;
 
-float ball_size = 0.1;
+float ball_size = 0.9;
 float ball_size2 = ball_size * ball_size;
 float ball_inv_size = 1 / ball_size;
 bool trace_values = false;
 
 Vector floor_color(0.14, 1.0, 0.14);
 Vector wall_color(0.85, 0.8, 0.48);
-Vector ceiling_color(0.88, 0.88, 0.88);
+Vector ceiling_color(0.98, 0.98, 0.98);
 Vector floor_normal(0, 0, 1);
 float ray_attenuation = 0.6;
 float defuse_attenuation = 0.4;
 
+std::vector<int> max_rays = {1, 1, 5, 1, 1, 1};
+int max_depth = 2;
+
 std::random_device rd;
 std::mt19937 gen(rd());
-std::normal_distribution<> distr{-0.07,0.07};
+std::normal_distribution<> distr{-0.02,0.02};
+
+int numCPU = sysconf(_SC_NPROCESSORS_ONLN);
 
 
 void print(const char* msg, const Vector& v) {
@@ -166,8 +174,9 @@ class Ball {
 
 std::vector<Ball> balls = {
   {{0, 0.7 * ball_size, ball_size}, {1, 0.01, 0.01}},
-  {{-2 * ball_size, 0. * ball_size, ball_size}, {0.01, 1.0, 0.01}},
-  {{2 * ball_size, 0. * ball_size, ball_size}, {1.00, 0.00, 1.}},
+  {{-2 * ball_size, 0, ball_size}, {0.01, 1.0, 0.01}},
+  {{2 * ball_size, 0, ball_size}, {1.00, 0.00, 1.}},
+  {{2 * ball_size, 0, ball_size}, {1.00, 1.00, 1.}},
 };
 
 optional<Point> trace_ball(const Vector& norm_ray, const Vector& origin, int depth) {
@@ -244,7 +253,6 @@ Point ball_light(
   return full_color;
 }
 
-
 Point floor_light(
     const Point& color,
     const Point& normal,
@@ -253,15 +261,18 @@ Point floor_light(
     int depth) {
   Vector total_color = black;
   if (depth > 0) {
-    Vector rand_reflection = Vector(
-        reflection.x + distr(gen),
-        reflection.y + distr(gen),
-        reflection.z + distr(gen)).normalize();
+    for (int i = 0; i < max_rays[depth]; i++) {
+      Vector rand_reflection = Vector(
+          reflection.x + distr(gen),
+          reflection.y + distr(gen),
+          reflection.z + distr(gen)).normalize();
 
-    auto res = trace(rand_reflection, point, depth - 1);
-    if (res) {
-      total_color += color.mul(*res) * ray_attenuation;
+      auto res = trace(rand_reflection, point, depth - 1);
+      if (res) {
+        total_color += color.mul(*res) * ray_attenuation;
+      }
     }
+    total_color *= (1. / max_rays[depth]);
   }
   Vector light_from_floor = light_pos - point;
   float light_distance2 = light_from_floor.size2();
@@ -378,7 +389,7 @@ optional<Point> trace(const Vector& norm_ray, const Vector& origin, int depth) {
 
 Vector sight = Vector(0., 1, -0.1).normalize();
 
-float dx = 0.1 / WINDOW_WIDTH;
+float dx = 1.9 / WINDOW_WIDTH;
 
 Vector sight_x = Vector(dx, 0, 0);
 Vector sight_y = (sight ^ sight_x).normalize() * dx;
@@ -401,10 +412,91 @@ Vector saturateColor(Point c) {
                 grey + scale * c.z);
 }
 
+
 void check_saturation() {
   auto s = saturateColor(Vector(2, 0.1, 0));
   P(s);
   assert(s.sum() == Vector(1, 0.6, 0.5).sum());
+}
+
+Point viewer = Point(0, -6, 2.0);
+
+std::vector<std::thread> threads;
+std::mutex m;
+std::condition_variable cv;
+int frame = 0;
+bool die = false;
+int num_running = 0;
+Uint8 *pixels;
+
+
+void drawThread(int id) {
+  Vector yoffset = sight_y * (WINDOW_HEIGHT / 2);
+  Vector xoffset = sight_x * (WINDOW_WIDTH / 2);
+  Uint8* my_pixels = pixels;
+
+  Vector yray = sight - yoffset - xoffset;
+  for (int y = 0; y < WINDOW_HEIGHT; y++) {
+    if (y % numCPU == id) {
+      Vector ray = yray;
+      for (int x = 0; x < WINDOW_WIDTH; x++) {
+        Vector norm_ray = ray.normalize();
+        trace_values = x == 500 && y == 500;
+        auto res = trace(norm_ray, viewer, max_depth);
+        if (res) {
+          Vector saturated = saturateColor(*res);
+          *my_pixels++ = colorToInt(saturated.x);
+          *my_pixels++ = colorToInt(saturated.y);
+          *my_pixels++ = colorToInt(saturated.z);
+          *my_pixels++ = 255;
+        }
+        ray += sight_x;
+      }
+    } else {
+      my_pixels += WINDOW_WIDTH * 4;
+    }
+    yray += sight_y;
+  }
+}
+
+void worker(int id) {
+  int my_frame = 0;
+  bool my_die;
+  while (true) {
+    my_frame++;
+    {
+      std::unique_lock<std::mutex> lk(m);
+      cv.wait(lk, [&my_frame]{return my_frame == frame;});
+      my_die = die;
+    }
+    if (!my_die) {
+      drawThread(id);
+    }
+    {
+      std::unique_lock<std::mutex> lk(m);
+      num_running--;
+      cv.notify_all();
+      if (my_die) return;
+    }
+  }
+}
+
+void draw() {
+  if (threads.size() == 0) {
+    for (int i = 0; i < numCPU; i++) {
+      threads.push_back(std::thread(worker, i));
+    }
+  }
+  {
+    std::unique_lock<std::mutex> lk(m);
+    num_running = threads.size();
+    frame++;
+    cv.notify_all();
+  }
+  {
+    std::unique_lock<std::mutex> lk(m);
+    cv.wait(lk, []{return num_running == 0;});
+  }
 }
 
 int main(void) {
@@ -419,37 +511,128 @@ int main(void) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-
-    Vector yoffset = sight_y * (WINDOW_HEIGHT / 2);
-    Vector xoffset = sight_x * (WINDOW_WIDTH / 2);
-    Point viewer = Point(0, -6, 0.8);
-    int max_depth = 4;
-
-    Vector yray = sight - yoffset - xoffset;
-    for (int y = 0; y < WINDOW_HEIGHT; y++) {
-      Vector ray = yray;
-      for (int x = 0; x < WINDOW_WIDTH; x++) {
-	Vector norm_ray = ray.normalize();
-        trace_values = x == 500 && y == 500;
-	auto res = trace(norm_ray, viewer, max_depth);
-	if (res) {
-          Vector saturated = saturateColor(*res);
-          SDL_SetRenderDrawColor(
-              renderer,
-              colorToInt(saturated.x),
-              colorToInt(saturated.y),
-              colorToInt(saturated.z), 255);
-          SDL_RenderDrawPoint(renderer, x, y);
-        }
-	ray += sight_x;
-      }
-      yray += sight_y;
-    }
+    SDL_Texture * texture = SDL_CreateTexture(renderer,
+                SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, WINDOW_WIDTH, WINDOW_HEIGHT);
+    pixels = new Uint8[WINDOW_WIDTH * WINDOW_HEIGHT * 4];
 
     SDL_RenderPresent(renderer);
+    bool move_forward = false;
+    bool move_backward = false;
+    bool turn_left = false;
+    bool turn_right = false;
+    unsigned int currentTime = SDL_GetTicks();
+
     while (1) {
-        if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+      unsigned int newTime = SDL_GetTicks();
+      unsigned int dt = newTime - currentTime;
+      if (move_forward || move_backward) {
+        Vector move = sight;
+        move.z = 0;
+        viewer += move * (0.001 * dt * (move_forward ? 1 : -1));
+        balls[3].position_ = viewer;
+        balls[3].position_.z = ball_size;
+      }
+
+      if (turn_left || turn_right) {
+        trace_values = true;
+        P(sight);
+        sight = (((sight ^ Vector(0,0,turn_left ? -1 : 1)) * (0.001 * dt) + sight)).normalize();
+        P(sight);
+        P(sight_x);
+        sight_x = sight ^ Vector(0,0,dx);
+        P(sight_x);
+        P(sight_y);
+        sight_y = (sight ^ sight_x).normalize() * dx;
+        P(sight_y);
+      }
+
+      while(SDL_PollEvent(&event)) {
+        switch (event.type) {
+          case SDL_QUIT:
+            exit(0);
             break;
+          case SDL_MOUSEBUTTONUP:
+                         if (event.button.button == SDL_BUTTON_LEFT)
+                           move_forward = false;
+                         break;
+          case SDL_MOUSEBUTTONDOWN:
+                         if (event.button.button == SDL_BUTTON_LEFT)
+                           move_forward = true;
+                         break;
+          case SDL_KEYDOWN:
+          case SDL_KEYUP:
+                         switch (event.key.keysym.scancode) {
+                           case SDL_SCANCODE_ESCAPE:
+                             die = true;
+                             draw();
+                             for (auto& t : threads) t.join();
+                             exit(0);
+                           case SDL_SCANCODE_W:
+                             move_forward = event.key.state == SDL_PRESSED;
+                             break;
+                           case SDL_SCANCODE_S:
+                             move_backward = event.key.state == SDL_PRESSED;
+                             break;
+                           case SDL_SCANCODE_A:
+                             turn_left = event.key.state == SDL_PRESSED;
+                             break;
+                           case SDL_SCANCODE_E:
+                           case SDL_SCANCODE_D:
+                             turn_right = event.key.state == SDL_PRESSED;
+                             break;
+                           case SDL_SCANCODE_1:
+                             max_depth = 1;
+                             break;
+                           case SDL_SCANCODE_2:
+                             max_depth = 2;
+                             break;
+                           case SDL_SCANCODE_3:
+                             max_depth = 3;
+                             break;
+                           case SDL_SCANCODE_5:
+                             max_rays[0] = 1;
+                             max_rays[1] = 1;
+                             max_rays[2] = 1;
+                             max_rays[3] = 1;
+                             max_rays[3] = 1;
+                             max_rays[max_depth] = 5;
+                             break;
+                           case SDL_SCANCODE_6:
+                             max_rays[0] = 1;
+                             max_rays[1] = 1;
+                             max_rays[2] = 1;
+                             max_rays[3] = 1;
+                             max_rays[3] = 1;
+                             break;
+                           case SDL_SCANCODE_7:
+                             distr = std::normal_distribution<double>{-0.00,0.00};
+                             break;
+                           case SDL_SCANCODE_8:
+                             distr = std::normal_distribution<double>{-0.03,0.03};
+                             break;
+                           case SDL_SCANCODE_9:
+                             distr = std::normal_distribution<double>{-0.3,0.3};
+                             break;
+                           case SDL_SCANCODE_MINUS:
+                             printf("Light size 0.1\n");
+                             light_size = 0.1;
+                             light_size2 = light_size * light_size;
+                             break;
+                           case SDL_SCANCODE_EQUALS:
+                             printf("Light size 2\n");
+                             light_size = 2;
+                             light_size2 = light_size * light_size;
+                             break;
+                         }
+        }
+      }
+
+      draw();
+      SDL_UpdateTexture(texture, NULL, (void*)pixels, WINDOW_WIDTH * sizeof(Uint32));
+      SDL_RenderCopy(renderer, texture, NULL, NULL);
+      SDL_RenderPresent(renderer);
+      event.type = -1;
+      currentTime = newTime;
     }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
